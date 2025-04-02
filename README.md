b1a804e4fa48aecdea0611f821d949b46bf2b1ef
lgzanoni <luiszanoni01@gmail.com>
2025-04-02 18:03:09 -0300

    feat( Construcao de webscraper com selenium )

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # RoboDoc
+
 Projeto de automatizacao em python
+
+Atividades:
+
+Automação de Processos e Bots;
+
+- Desenvolvimento de robôs para automação de tarefas em aplicações web, desktop e sistemas legados;
+
+- Utilização de OCR (Reconhecimento Óptico de Caracteres) para capturar informações em telas não estruturadas (Tesseract OCR, OpenCV);
+
+- Automação de interações com APIs RESTful e SOAP para integração entre sistemas.
+
+Programação em Python;
+
+- Domínio de bibliotecas como PyAutoGUI, Selenium, BeautifulSoup4, Pandas, OpenPyXL, Requests, Pywinauto.
+
+- Experiência na manipulação de arquivos (Excel, CSV, JSON, XML, PDF);
+
+- Estruturação de código modular e reutilizável para automações escaláveis.
+
+Requisitos:
+
+Conhecimento em bancos de dados SQL (MySQL, PostgreSQL, SQL Server) e NoSQL (MongoDB).
+Experiência com orquestradores RPA como Jenkins.
+Implementação de logs e alertas para rastreamento de execução e erros.
+Conhecimento em ferramentas de observabilidade como Grafana.
---
+++ automation/web_scraper.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+import psycopg2
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from datetime import datetime
+
+print("#################################")
+print("INICIANDO PROCESSO AUTOMATIZADO DE BUSCA DE PRECOS COM SELENIUM")
+print("#################################")
+
+connection = psycopg2.connect(database="postgres",
+                        host="localhost",
+                        user="postgres",
+                        password="postgres",
+                        port="5432")
+
+if(connection):
+    print("Conectado ao banco de dados Postgres")
+
+query = connection.cursor()
+
+chrome_options = Options()
+
+
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get("https://www.magazineluiza.com.br/")
+
+print(driver.title)
+
+search_bar = driver.find_element(By.CSS_SELECTOR, '[data-testid="input-search"]')
+search_bar.send_keys("Galaxy zflip 6")
+
+submit_button = driver.find_element(By.CSS_SELECTOR, '[data-testid="search-submit"]')
+
+submit_button.click()
+
+driver.implicitly_wait(10)
+
+primeiro_objeto = driver.find_element(By.XPATH, '//ul[@data-testid="list"]/li[1]')
+valor = primeiro_objeto.find_element(By.XPATH, '//p[@data-testid="price-value"]')
+
+
+produto = "Galaxy ZFlip 6"
+data = datetime.now()
+query.execute(f"INSERT INTO precos (produto, preco, data) VALUES (\'{produto}\', \'{valor.text}\', \'{data}\')")
+connection.commit()
+
+print(valor.text)
