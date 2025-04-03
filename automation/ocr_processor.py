23ba7cf551d234cdd6c58fe7a7aee212e124e98b
lgzanoni <luiszanoni01@gmail.com>
2025-04-03 15:25:39 -0300

    feat( Leitura de Curriculo.png com Tesseract)

--- automation/ocr_processor.py
+++ automation/ocr_processor.py
@@ -1,1 +1,12 @@
 # Tesseract OpenCV
+from PIL import Image
+import pytesseract
+from pytesseract import Output
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+imagem = Image.open("Curriculo.png")
+texto = pytesseract.image_to_string(imagem, lang="por")
+dados = pytesseract.image_to_data(imagem, lang="por", output_type=Output.DICT)
+
+print(texto)
