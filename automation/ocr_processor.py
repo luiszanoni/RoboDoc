40a98ba9e08de04331f34130c672f6d03ce0ebd9
lgzanoni <luiszanoni01@gmail.com>
2025-04-03 15:38:47 -0300

    feat( Conferindo outras funcoes do Tesseract )

--- automation/ocr_processor.py
+++ automation/ocr_processor.py
@@ -6,7 +6,17 @@ from pytesseract import Output
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 imagem = Image.open("Curriculo.png")
+
 texto = pytesseract.image_to_string(imagem, lang="por")
 dados = pytesseract.image_to_data(imagem, lang="por", output_type=Output.DICT)
+boxes = pytesseract.image_to_boxes(imagem, lang="por")
+osd = pytesseract.image_to_osd(imagem)
+idiomas = pytesseract.get_languages()
+
 
 print(texto)
+# print(dados)
+# print(boxes)
+# print(osd)
+# print(idiomas)
+
